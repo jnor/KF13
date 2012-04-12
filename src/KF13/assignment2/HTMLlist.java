7a49fef70e0814c410ac27f70435a275e3102506
Jonas Norlin <jonas@norlins.dk>
2012-04-12 14:53:49 +0200

    htmllist

---
+++ src/KF13/assignment2/HTMLlist.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KF13.assignment2;
+
+/**
+ *
+ * @author jn
+ */
+public class HTMLlist {
+    
+    String str;
+    HTMLlist next;
+
+    HTMLlist (String s, HTMLlist n) {
+        str = s;
+        next = n;
+    }
+    
+}
